8fe48412cff3bfac7956b1d48fcaa98d2c51c105
robertotics4 <robertooliveiragp@gmail.com>
2025-02-19 00:20:42 -0300

    test: ignore main test

--- src/test/java/com/amcom/desafiotecnicoamcom/DesafioTecnicoAmcomApplicationTests.java
+++ src/test/java/com/amcom/desafiotecnicoamcom/DesafioTecnicoAmcomApplicationTests.java
@@ -1,13 +1,25 @@
 package com.amcom.desafiotecnicoamcom;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Disabled
 class DesafioTecnicoAmcomApplicationTests {
+
+    @Autowired
+    private ApplicationContext context;
 
     @Test
     void contextLoads() {
+        assertNotNull(context);
     }
 
 }
